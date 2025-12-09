7433621d70d9b8ea757c2044a2628a7197f1a882
Rahul Yedida <yrahul3910@gmail.com>
2025-12-09 01:28:34 -0500

    add day9 part 1

--- day09/solve.go
+++ day09/solve.go
@@ -1,8 +1,57 @@
 package day09
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Coordinate struct {
+	x int
+	y int
+}
+
+func (c Coordinate) String() string {
+	return fmt.Sprintf("(%d, %d)", c.x, c.y)
+}
+
+func ParseInput(input string) []Coordinate {
+	arr := make([]Coordinate, 0)
+
+	for line := range strings.SplitSeq(input, "\n") {
+		if len(strings.TrimSpace(line)) == 0 {
+			continue
+		}
+
+		parts := strings.Split(line, ",")
+
+		x, _ := strconv.ParseInt(parts[0], 10, 64)
+		y, _ := strconv.ParseInt(parts[1], 10, 64)
+
+		arr = append(arr, Coordinate{int(x), int(y)})
+	}
+
+	return arr
+}
+
 func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+	coords := ParseInput(input)
+	maxArea := 0
+
+	for i := range len(coords) {
+		for j := i + 1; j < len(coords); j++ {
+			curX := float64(coords[i].x - coords[j].x)
+			curY := float64(coords[i].y - coords[j].y)
+			curArea := int(math.Abs(curX)+1) * int(math.Abs(curY)+1)
+
+			if curArea > maxArea {
+				maxArea = curArea
+			}
+		}
+	}
+
+	return maxArea
 }
 
 func SolvePuzzle2(input string) int {
